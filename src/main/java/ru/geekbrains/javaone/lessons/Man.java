dcd94c14ba3b6b42224802d50964c32b43c35591
CherryBlond <55181421+CherryBlond@users.noreply.github.com>
2020-04-05 23:01:26 +0300

    Revert "Maven package"

--- src/main/java/ru/geekbrains/javaone/lessons/Man.java
+++
@@ -1,8 +0,0 @@
-package ru.geekbrains.javaone.lessons;
-
-public class Man extends Participants implements Actions {
-    {
-        maxRunLength = 500;
-        maxJumpHeight = 1;
-    }
-}
--- src/main/java/ru/geekbrains/javaone/lessons/Robot.java
+++
@@ -1,8 +0,0 @@
-package ru.geekbrains.javaone.lessons;
-
-public class Robot extends Participants implements Actions {
-    {
-        maxRunLength = 100;
-        maxJumpHeight = 3;
-    }
-}
